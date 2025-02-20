60923e07914752f5f5ea317af127a719ee32ad7a
MarianaOrtegaRam <m.ortegar@uniandes.edu.co>
2025-02-20 07:52:10 -0500

    avnace

--- Problema2.py
+++ Problema2.py
@@ -7,6 +7,21 @@ model.O = RangeSet (o) # Ciudades origen
 model.D = RangeSet (d) # Ciudades destino
 model.x = Var ( model.O , model.D , within = NonNegativeReals )
 
+c_values = {(1,1):0,
+            (1,2):2.5,
+            (1,3):1.6,
+            (1,4):1.4,
+            (1,5):0.8,
+            (1,6):1.4,
+            (2,1):2.5,
+            (2,2):0,
+            (2,3):2.0,
+            (2,4):1.0,
+            (2,5):1.0,
+            (2,6):0.8,
+
+}
+model.cij = Var(model.O, model.D, within=NonNegativeReals, initialize=c_values)
 
 model.obj = Objective ( expr = sum (c[i , j ] * model .x [i , j] for
 i in model .I for j in model .J) , sense = minimize )
