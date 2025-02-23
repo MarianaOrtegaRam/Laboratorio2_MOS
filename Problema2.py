1b9c5bef7b8d4ddbe4ab0b49c6229fdfd1bdcc2d
MarianaOrtegaRam <m.ortegar@uniandes.edu.co>
2025-02-23 17:40:08 -0500

    problema 2 listo

--- Problema2.py
+++ Problema2.py
@@ -5,7 +5,7 @@ o = 2
 d = 6
 model.O = RangeSet (o) # Ciudades origen
 model.D = RangeSet (d) # Ciudades destino
-model.x = Var ( model.O , model.D , within = NonNegativeReals )
+model.x = Var (model.O,model.D, within = NonNegativeReals)
 
 c_values = {(1,1):0,
             (1,2):2.5,
@@ -18,10 +18,40 @@ c_values = {(1,1):0,
             (2,3):2.0,
             (2,4):1.0,
             (2,5):1.0,
-            (2,6):0.8,
+            (2,6):0.8}
 
-}
-model.cij = Var(model.O, model.D, within=NonNegativeReals, initialize=c_values)
+l_values ={1: 550,
+           2:700}
 
-model.obj = Objective ( expr = sum (c[i , j ] * model .x [i , j] for
-i in model .I for j in model .J) , sense = minimize )
+d_values ={1:125,
+           2:175,
+           3:225,
+           4:250,
+           5:225,
+           6:200}
+
+model.cij = Param(model.O, model.D, within=NonNegativeReals, initialize=c_values)
+model.li = Param(model.O, within=NonNegativeReals, initialize=l_values)
+model.dj = Param(model.D, within=NonNegativeReals, initialize=d_values)
+
+model.obj = Objective (expr= sum (model.cij[i,j] * model.x[i,j] for i in model.O for j in model.D), sense= minimize )
+
+model.suministro = ConstraintList ()
+for i in model.O:
+    model.suministro.add(sum( model.x[i,j] for j in model.D) <= model.li[i])
+
+model.demanda = ConstraintList ()
+for j in model.D :
+    model.demanda.add(sum(model.x[i,j] for i in model.O ) == model.dj[j])
+
+model.restricciones_prohibidas = ConstraintList()
+model.restricciones_prohibidas.add(model.x[1,1] == 0)
+model.restricciones_prohibidas.add(model.x[2,2] == 0)
+
+from pyomo . opt import SolverFactory
+solver = SolverFactory ('glpk')
+solver.solve (model)
+
+for i in model.O :
+    for j in model.D :
+        print ( f"x[{i},{j}]={model.x[i,j].value}" )
